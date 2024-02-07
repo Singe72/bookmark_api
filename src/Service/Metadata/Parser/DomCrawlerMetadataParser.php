8d96344718f1014ab9e44a6ceab737ce92c6c973
Thomas Hyaumet <t.hyaumet@free.fr>
2024-02-07 10:17:07 +0100

    fix DomCrawler in getMetadata

--- src/Service/Metadata/Parser/DomCrawlerMetadataParser.php
+++ src/Service/Metadata/Parser/DomCrawlerMetadataParser.php
@@ -8,11 +8,11 @@ class DomCrawlerMetadataParser implements MetadataParserInterface
 {
     public function getMetadata(string $url, string $content): array
     {
-        $crawler = new Crawler(null, $url, useHtml5Parser: true);
-        $title = $crawler->filterXPath("//title")->text();
-        $description = $crawler->filterXPath("//meta[@name='description']")->attr("content");
-        $image = $crawler->filterXPath("//meta[@property='og:image']")->attr("content");
-        $language = $crawler->filterXPath("//html")->attr("lang");
+        $crawler = new Crawler($content, $url, useHtml5Parser: true);
+        $title = $crawler->filterXPath("//title")->text("No title found");
+        $description = $crawler->filterXPath("//meta[@name='description']")->attr("content", "No description found");
+        $image = $crawler->filterXPath("//meta[@property='og:image']")->attr("content", "No image found");
+        $language = $crawler->filterXPath("//html")->attr("lang", "No language found");
 
         return [
             "title" => $title,
